090171f14806b8fe1fea4817b849bae4bf013df9
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2024-04-10 13:52:24 +0500

    added auto id filling

--- background.js
+++ background.js
@@ -6,6 +6,7 @@ chrome.webRequest.onBeforeRequest.addListener(
       url.pathname.startsWith("/api/user-activities/")
     ) {
       const activityJson = loadLocalJsonFile("jsons/activity.json");
+      activityJson["user-activities"][0]["id"] = getUserId(url.pathname);
 
       return {
         redirectUrl:
@@ -19,6 +20,11 @@ chrome.webRequest.onBeforeRequest.addListener(
       const activityJson = loadLocalJsonFile("jsons/activity.json");
       const summariesJson = loadLocalJsonFile("jsons/summaries.json");
 
+      summariesJson["user-activity-summaries"][0]["id"] = getUserId(
+        url.pathname
+      );
+
+      // fill pins array of summariesJson with last 7 values of activityJson's pins array
       summariesJson["user-activity-summaries"][0]["pins"] = activityJson[
         "user-activities"
       ][0]["pins"].slice(0, 7);
@@ -39,4 +45,9 @@ const loadLocalJsonFile = (filename) => {
   xhr.open("GET", chrome.runtime.getURL(filename), false);
   xhr.send();
   return JSON.parse(xhr.responseText);
+};
+
+const getUserId = (pathname) => {
+  const parts = pathname.split("/");
+  return parseInt(parts[parts.length - 1]);
 };
